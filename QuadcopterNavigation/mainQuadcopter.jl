c2ca918e6e8abade0d02b920a53ffdd5b0cff2cb
GXZ <xiaozhan@control.ee.ethz.ch>
2018-04-27 00:05:42 -0700

    bug fixed

--- QuadcopterNavigation/mainQuadcopter.jl
+++ QuadcopterNavigation/mainQuadcopter.jl
@@ -1,6 +1,6 @@
 ###############
 # OBCA: Optimization-based Collision Avoidance - a path planner for autonomous parking
-# Copyright (C) 2017 
+# Copyright (C) 2018
 # Alexander LINIGER [liniger@control.ee.ethz.ch; Automatic Control Lab, ETH Zurich]
 # Xiaojing ZHANG [xiaojing.zhang@berkeley.edu; MPC Lab, UC Berkeley]
 #
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############
 # The paper describing the theory can be found here:
-# 	X. Zhang, A. Liniger and F. Borrelli; "Optimization-Based Collision Avoidance"; Technical Report, 2017
+# 	X. Zhang, A. Liniger and F. Borrelli; "Optimization-Based Collision Avoidance"; Technical Report, 2017, [https://arxiv.org/abs/1711.03449]
 ###############
 
 ###############
@@ -28,13 +28,10 @@
 
 
 
+
 # function defined in setup.jl
 clear()
-
-# horizon
-N  = 80
-# nominal sampling time
-Ts = 0.25
+close("all")
 
 egoR = 0.25
 
@@ -49,42 +46,112 @@ ob52 = [7.5,5, 7,   -7,-4,-3]
 ob6  = [10,10, 5, 0,0, 0]
 
 # x0 = [1,Y,Z,0,0,0] Y and Z can be modified
-x0 = [1 1 3 0 0 0 0 0 0 0 0 0]
+x0 = [1 1 3 0 0 0 0 0 0 0 0 0]	
 # xF = [9,Y,Z,0,0,0] Y and Z can be modified
-xF = [9 3 2 0 0 0 0 0 0 0 0 0]
+xF = [9 3 2 0 0 0 0 0 0 0 0 0]	
+
+# reshape the state
+Ts = 0.25
+timeScalep = 0.5;
+
+################## construct environment for hybrid A-star ##################
+### note: environment scaled by 10x for convenience ###
+ox = Float64[]
+oy = Float64[]
+oz = Float64[]
+# first wall
+for xx in 20 : 25
+	for yy in 0 : 105
+		for zz in 6 : 55
+			push!(ox,Float64(xx))
+			push!(oy,Float64(yy))
+			push!(oz,Float64(zz))	
+		end
+	end	
+end
+# second wall; only for plotting purposes
+for xx = 70 : 75
+	# left piece
+	for yy = 0 : 40
+		for zz = 0 : 55
+			push!(ox, Float64(xx))
+			push!(oy, Float64(yy))
+			push!(oz, Float64(zz))
+		end
+	end
+	# right piece
+	for yy = 50 : 105
+		for zz = 0 : 55
+			push!(ox, Float64(xx))
+			push!(oy,Float64(yy))
+			push!(oz,Float64(zz))
+		end
+	end
+	# top piece
+	for yy = 40 : 50
+		for zz = 30 : 55
+			push!(ox, Float64(xx))
+			push!(oy,Float64(yy))
+			push!(oz,Float64(zz))
+		end
+	end
+	# right piece
+	for yy = 40 : 50
+		for zz = 0 : 20
+			push!(ox, Float64(xx))
+			push!(oy,Float64(yy))
+			push!(oz,Float64(zz))
+		end
+	end
+end
+# room size
+xmin = 0.0
+ymin = 0.0
+zmin = 0.0
+xmax = 105.0
+ymax = 105.0
+zmax = 55.0
+# extract start and end position
+sx = x0[1]*10.0  # [m]
+sy = x0[2]*10.0  # [m]
+sz = x0[3]*10.0  # [m]
+gx = xF[1]*10.0  # [m]
+gy = xF[2]*10.0  # [m]
+gz = xF[3]*10.0  # [m]
+# call A* algorithm
+rx, ry, rz, rtime = a_star.calc_astar_path( sx, sy, sz, 		# start
+									gx, gy, gz, 		# goal
+									ox, oy, oz, 		# list of obstacles
+									xmin, ymin, zmin, 	# box constraint
+									xmax, ymax, zmax, 	# box constraint
+									1.0	)				# (scaled) grid resolution 
+									
+N_as = length(rx)-1	# length of Astar
+# nominal sampling time
+Ts_as = round((Ts*80/N_as)*100)/100	# scale sampling time for Astar
+
+###### stitch together Astar solution for warm starting ######
+rxryrz=[rx'/10 ; ry'/10 ; rz'/10]
+vWS = zeros(3,N_as+1)
+xWS_as = [rx'/10 ; ry'/10 ; rz'/10 ; zeros(3,N_as+1) ; vWS ; zeros(3,N_as+1) ];
+uWS_as = 0.5*ones(4,N_as);	# same length as horizon
+timeWS_as = 1
+# not plotting might get rid of IPOPT restoration failure messages...
+plotTrajQuadcopter(xWS_as',uWS_as',N_as,egoR,ob12,ob22,ob32,ob42,ob52,ob6,egoR,"Warm Start (A*) ",0)
 
 
-xWS = zeros(12,N+1)
-uWS = zeros(4,N)
-timeWS = 1;
-
-xp1 = zeros(12,N+1)
-up1 = zeros(4,N)
-scaleTime1 = 1
-
-xp2 = zeros(12,N+1)
-up2 = zeros(4,N)
-scaleTime2 = 1
-exitflag = -1
+######### trajectory with Distance Approach
+println("Trajectory using Distance Approach (Collision Avoidance, A star)")
+xp1_as,up1_as,scaleTime1_as,exitflag1_as,time1_as,l1_as,exitstatus1_as  = QuadcopterDist(x0,xF,N_as,Ts_as,egoR,ob12,ob22,ob32,ob42,ob52,xWS_as,uWS_as,timeWS_as)
+plotTrajQuadcopter(xp1_as',up1_as',N_as,egoR,ob12,ob22,ob32,ob42,ob52,ob6,egoR,"Collision Avoidance with distance approach ",10)
+trajFeas1_as = constrSatisfaction(xp1_as,up1_as,scaleTime1_as,x0,xF,Ts_as,l1_as,ob12,ob22,ob32,ob42,ob52,egoR) 
 
 
-# warm starting trajectory
-println("Warm starting trajectory")
-xWS, uWS, timeWS = WarmStartQuadcopter(N,Ts,x0,xF)
-close("all")
-plotTrajQuadcopter(xWS',uWS',N,egoR,ob12,ob22,ob32,ob42,ob52,ob6,egoR,"Warm Start")
+######### trajectory with Signed Distance Approach
+println("Trajectory using Signed Distance Approach (Minimum Penetration, A star)")
+xp2_as,up2_as,scaleTime2_as,exitflag2_as,time2_as,l2_as,exitstatus2_as = QuadcopterSignedDist(x0,xF,N_as,Ts_as,egoR,ob12,ob22,ob32,ob42,ob52,xWS_as,uWS_as,timeWS_as)
+plotTrajQuadcopter(xp2_as',up2_as',N_as,egoR,ob12,ob22,ob32,ob42,ob52,ob6,egoR,"Minimum Penetration with signed-distance approach",20)
+trajFeas2_as = constrSatisfaction(xp2_as,up2_as,scaleTime2_as,x0,xF,Ts_as,l2_as,ob12,ob22,ob32,ob42,ob52,egoR) 
 
-# trajectory with Distance Approach
-println("Trajectory using Distance Approach (Collision Avoidance)")
-xp1,up1,scaleTime1,exitflag1,time1 = QuadcopterDist(x0,xF,N,Ts,egoR,ob12,ob22,ob32,ob42,ob52,xWS,uWS,timeWS)
-close("all")
-plotTrajQuadcopter(xp1',up1',N,egoR,ob12,ob22,ob32,ob42,ob52,ob6,egoR,"Collision Avoidance with distance approach")
-
-
-# trajectory with Signed Distance Approach
-println("Trajectory using Signed Distance Approach (Minimum Penetration)")
-xp2,up2,scaleTime2,exitflag2,time2 = QuadcopterSignedDist(x0,xF,N,Ts,egoR,ob12,ob22,ob32,ob42,ob52,xWS,uWS,timeWS)
-close("all")
-plotTrajQuadcopter(xp2',up2',N,egoR,ob12,ob22,ob32,ob42,ob52,ob6,egoR,"Minimum Penetration with signed-distance approach")
-
+println("---- Done ----")
 
